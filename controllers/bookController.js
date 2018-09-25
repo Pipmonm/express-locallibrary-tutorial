93b2b68671f306cb9acb757cbf2efd8ff13c4ca8
Pipmon <pipmonm@netscape.net>
2018-09-25 14:21:14 -0400

    Title change

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,7 +28,7 @@ exports.index = function(req, res) {
             Genre.count(callback);
         },
     }, function(err, results) {
-        res.render('index', { title: 'Local Library Home', error: err, data: results });
+        res.render('index', { title: 'Math For All', error: err, data: results });
     });
 };
 
