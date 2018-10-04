70b6b4528d88c8d3a37c084da634f2df02bbf6b0
Pipmon <pipmonm@netscape.net>
2018-10-04 14:58:41 -0400

    pslcr screenshot

--- controllers/pieSlicerController.js
+++ controllers/pieSlicerController.js
@@ -3,25 +3,34 @@ const { sanitizeBody } = require('express-validator/filter');
 
 var async = require('async');
 //var mongoose = require('mongoose'); // added  :MOD: 2018-03-08 10:32 AM
-
+exports.getObjects = function (req, res) {
+    var item = req.body;
+    var params = { Bucket: req.params.bucketName, Key: 'keyname'}; // keyname can be a filename
+    s3.getObject(params, function (err, data) {
+        if (err) {
+            return res.send({ "error": err });
+        }
+        res.send({ data });
+    });
+}
 
 //Display unique page details for PieSlicer
 exports.pieSlicer_detail = function(req, res) {
   console.log("trying for pieSlicerDetails");
-  let PieSlicerDesc1 = "<pre style='color:yellow; background:green'>Pie Slicer is an interactive visual aid application to help students visualize \n" +
-                    "and fully understand the math operations of fraction addition/subtraction.  \n" +
-                    "Students are able to easily manipulate pie pieces representing any fraction \n" +
-                    "problem and discover on their own the solutions and the central concepts behind \n " +
-                    "fraction addition and subtraction. \n" +
+  let PieSlicerDesc1 = "<pre style='color:yellow; background:green'>Pie Slicer is an interactive visual aid application to help students  \n" +
+                    "fully understand the math operations of fraction addition/subtraction.  \n" +
+                    "By easily manipulating pie pieces representing any fraction students are  \n" +
+                    "very often able to discover on their own the solutions and the central concepts \n " +
+                    "relating to fraction addition and subtraction. \n" +
                     "\n" +
-                    "As an exerciser, it fulfills one of the most important requirements in ensuring  \n" +
-                    "optimal retention and improvement of any new skill while using strong visual impact.\n" +
-                    "  - problems are generated randomly for different levels  \n" +
-                    "  - students' answers are verified immediately \n" +
-                    "  - the visual representation is updated automatically for correct answers \n" +
-                    "  - tracks users performance at each level \n" +
-                    "  - a dedicated help window is available for questions about the application itself or \n" +
-                    "    concerning the topic under study."
+                    "As can be noted on the screenshot shown above of the application  \n" +
+                    "Pie Slicer will: \n" +
+                    "  - generate problems randomly for different levels  \n" +
+                    "  - verify student answers immediately \n" +
+                    "  - update the visual representation automatically for correct answers \n" +
+                    "  - track users performance at each level \n" +
+                    "  - provide a dedicated help window for questions about the  \n" +
+                    "    application itself or concerning the topic under study."
                     "\n" +
                     "Download the Demo version ( no obligations ) to find out more.\n" +
                     "The demo will be functional for a limited time only.\n" +
