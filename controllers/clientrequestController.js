c28719a6f1173ab37ea657d26a70c928b0ede63a
Pipmon <pipmonm@netscape.net>
2018-11-19 17:03:49 -0500

    integrated client and clientrequest

--- controllers/clientrequestController.js
+++ controllers/clientrequestController.js
@@ -165,10 +165,9 @@ exports.clientrequest_delete_get = function(req, res, next) {
       //async.parallel({key1:func,key2:func},function(err,results))
       async.parallel({
           clientrequest: function(callback) { //was author:...
-              Client = require('../models/client');
               ClientRequest.findById(req.params.id)
               .populate('client')
-              .exec(callback)
+              //.exec(callback)
           },
           //cliens_clientrequests: function(callback) {
             //Client.find({ 'author': req.params.id }).exec(callback)
