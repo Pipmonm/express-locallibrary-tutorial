7ca7f522ab245655e700f7487f2cac013c2bfb9a
Pipmon <pipmonm@netscape.net>
2018-11-22 10:25:30 -0500

    on to client delete

--- controllers/clientrequestController.js
+++ controllers/clientrequestController.js
@@ -12,7 +12,7 @@ var debug = require('debug');
 // Display list of all ClientRequests.
 exports.clientrequest_list = function(req, res, next) {
   console.log('@@@ $ at clientrequest_list');
-  ClientRequest.find({}, 'title client')
+  ClientRequest.find({}, 'status client')
     .populate('client')
     .exec(function (err, list_clientrequests) {
       if (err) { return next(err); }
