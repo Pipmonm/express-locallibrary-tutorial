12647f1e4ed6cf7b2d0ad2ddb36bba954db9d127
Pipmon <pipmonm@netscape.net>
2018-11-23 12:01:45 -0500

    why JSON object in appname model?

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -49,7 +49,7 @@ exports.genre_detail = function(req, res, next) {
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
+exports.genre_create_get = function(req, res, next) {
     res.render('genre_form', { title: 'Create Genre' });
   };
 
