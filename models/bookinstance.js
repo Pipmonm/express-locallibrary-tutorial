c2c4510413863b6546e2770613234fe1fece0a2b
Pipmon <pipmonm@netscape.net>
2018-11-23 18:02:53 -0500

    hunting of the SNARK

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -6,8 +6,8 @@ var Schema = mongoose.Schema;
 var BookInstanceSchema = new Schema(
 
   {
-    book: { type: Schema.ObjectId, ref: 'Book', required: true }, //reference to the associated book
-    //book: { type: mongoose.ObjectId, ref: 'Book', required: true },
+    //book: { type: Schema.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    book: { type: mongoose.ObjectId, ref: 'Book', required: true },
     imprint: {type: String, required: true},
     status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
     due_back: {type: Date, default: Date.now}
