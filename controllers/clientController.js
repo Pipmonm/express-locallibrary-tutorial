181575d9fc0c8d22125f264970a312966d8e32cf
Pipmon <pipmonm@netscape.net>
2018-12-01 21:52:49 -0500

    refactoring many things

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -105,10 +105,10 @@ exports.client_status_post = [
            key = key ^ result;//done at server and sent to client
            console.log("licenseKey is: " + key.toString());
 
-           Client.findByIdAndUpdate(doc._id, { license_string: 'License is:', license_key: key.toString() }, function(err,newdoc){
+           Client.findByIdAndUpdate(doc._id, {license_string: 'License is:', license_key: key.toString() }, function(err,newdoc){
                console.log("@@@ $ post client update  client: >v");
                console.log(newdoc)
-               res.render('client_licensekey', {title: 'License Key Details', client: newdoc});
+               //res.render('client_licensekey', {title: 'License Key Details', client: newdoc});
             });
          });//end callback
       };//end if clause
