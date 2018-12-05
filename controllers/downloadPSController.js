bbb907bb4323c69fc7e5d70683d2720b2043fad3
Pipmon <pipmonm@netscape.net>
2018-12-05 18:42:33 -0500

    refactoring many things

--- controllers/downloadPSController.js
+++ controllers/downloadPSController.js
@@ -11,23 +11,21 @@ var async = require('async');
 exports.downloadPS_view = function(req, res) {
   console.log("specifics of PS downloads");
   let downloadPSDesc1 = "<pre style='color:yellow; background:green; width:660px'> \n" +
-  "PieSlicerDual.exe application is restricted to the specific computer (Windows based only)  \n"+
-  "whose system ID and Format Code have been used to obtain a license. (Key Code)  \n"+
-  "Therefore be sure to run the demo on the computer that is to be used by whomever is to  \n"+
-  "be using the application.  (cf below for USB version) \n "+
+  "PieSlicerDual.exe application is limited to the specific computer (Windows based only)  \n"+
+  "whose system ID and Format Code are used when obtaining the license key.  \n"+
+  "Therefore be sure to get this data from the computer that is to be used ultimately.  \n"+
+  " (cf below for USB version) \n "+
   "  \n"+
-  "The 'Registration' tab on the opening page of FracSpeller will display both the System ID \n"+
-  "and a Format Code required for registration purposes.  They are automatically placed \n"+
-  "in your system's clipboard upon cancelling the message that displays them.  \n"+
-  "Paste this information in the indicated space when using this site's \"PURCHASE FORM\" page \n"+
-  "as the first step in obtaining a fully functional registered copy.\n" +
+  "The System ID and Format Code are placed in your system's clipboard when you click on the \n"+
+  "\"REGISTRATION\" tab shown on the opening page of the demo application. \n"+
+  "Paste this information where requested when filling this site's \"PURCHASE FORM\" page \n"+
   "  \n"+
   "USB Version: \n" +
   "The application may also be placed on a USB drive allowing use on any computer. \n" +
-  "However the USB version does not provide a \"DEMO\" mode.  View the DEMO first on a CPU or \n" +
-  "Notebook, (running WINDOWS only) then, if you prefer a USB based version, move the .exe file to\n" +
-  "a USB.  Run it there and use the same procedure as described above to register the application.\n" +
-  "The complete USB version will only run when you enter its unique Key Code. \n" +
+  "The USB version does not provide a \"DEMO\" mode.  View the DEMO first on a CPU or Notebook, \n" +
+  "(running WINDOWS only) then, if you prefer a USB based version, move the .exe file to a USB.\n" +
+  "Run it there and follow the same procedure as described above to register the application.\n" +
+  "The USB version will not run until you enter its unique Key Code. \n" +
   " \n" +
   "    NOTE CAREFULLY: Some USB drives do not have a serial id and cannot be used.\n"+
   "                    Message 'UNKNOWN DRIVE' will alert you to this condition.\n"+
