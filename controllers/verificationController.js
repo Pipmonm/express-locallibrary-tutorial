9654938642dfe4568890aabb6afb8fb7c04904e5
Pipmon <pipmonm@netscape.net>
2019-01-15 16:25:40 -0500

    Adding SHA-256 hash codes part 2

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -11,28 +11,29 @@ exports.verify_view = function(req, res) {
   "Microsoft Windows can perform the verification of a file's authenticity by using its\n" +
   "'PowerShell' utility command 'FCI'. \n"+
   "Steps:\n"+
-  "1- Type 'PowerShell' in the Search Command Box (bottom left of Window screen)\n"+
-  "2- Select the first option (something like: 'Windows PowerShell ISE (x86)' ) \n"+
-  "  (Next steps use file 'PieSlicer.exe' downloaded onto the desktop as an example,)\n"+
-  "3- At PowerShell prompt (probably 'PS C:>' but not important ) type in: \n"+
-  "   PS C:> \"Get-FileHash C:\Users\User\Desktop\PieSlicer.exe\"  + do Carriage Return (CR)\n"+
+  "1- Type 'PowerShell' in the Search Command Box (white box bottom left of screen)\n"+
+  "2- Click the first option ('Windows PowerShell ISE (x86)' or similar ) \n"+
+  "  (Following is an example based on 'PieSlicer.exe' downloaded onto the desktop)\n"+
+  "3- At PowerShell prompt (probably 'PS C:>' but OK if different) type in: \n"+
+  "   PS C:> \"Get-FileHash C:\Users\User\Desktop\PieSlicer.exe\"  + press 'ENTER' (aka CR)\n"+
   "\n"+
-  "  PowerShell will then print out a very long hash code (as in following example)\n"+
+  "  PS will then print out a very long hash code (as in following example)\n"+
   "     HASH part:>  5B1332C19F156E8CCEBF8F7FD749F8D3D23CB30685BC52A35A4FA3C8F6EFCBE8 \n" +
   "\n"+
-  " Next compare this Hash code with the one from our site:\n"+
+  " Next need to compare this Hash code with the one from our site:\n"+
   " In PowerShell\n"+
-  "4- Double click right on the hash code shown (turns selected) then Right-Click & copy\n"+
-  "5- Next, at PowerShell prompt, type: \"compare\" \n"+
-  "  PowerShell will give a blurb and end with a prompt asking for first item to be compared\n"+
-  "  ReferenceObject[0]: 5B1332C19F156..... (use rightClick to paste hash code here) \n"+
-  "  ReferenceObject[1]: (paste nothing, do CR)\n"+
-  "  DifferenceObject[0]: (paste SHA-256 Hash Code for 'PieSlicer.exe' as given on our site)\n"+
-  "  DifferenceObject[1]: (paste nothing, do CR)\n"+
+  "4- Double click directly on the hash code (turns highlighted) then Right-Click on it & copy\n"+
+  "5- Move to active PS prompt (PS C:>) and type: \"compare\" \n"+
+  "  PowerShell will request information as shown below (including your responses):\n"+
+  "            prompt      your response \n"+
+  "  ReferenceObject[0]: 5B1332C19F156..... (use rightClick to paste hash code from step 4 here) \n"+
+  "  ReferenceObject[1]: (press ENTER)\n"+
+  "  DifferenceObject[0]: (paste SHA-256 Hash Code for 'PieSlicer.exe' as shown on our web site)\n"+
+  "  DifferenceObject[1]: (press ENTER)\n"+
   "  PS C:>\n"+
   "\n"+
   "  If codes are exactly the same, only the normal prompt, as shown above, is returned.\n"+
-  "  If some codes are shown with '=<' comparisons or similar then files are different.\n"+
+  "  If hash codes are shown, with '=<' comparisons or similar, then files are different.\n"+
   "  This normally comes from a download error or file corruption error.\n"+
   "  Do not run any file with an error, attempt to download again, or contact us.\n"+
   "</pre>"
