c755a6c77c23a0f20c0f97647756c77541f799b5
Pipmon <pipmonm@netscape.net>
2019-01-17 14:40:06 -0500

    Adding SHA-256 hash codes part 3

--- controllers/aboutUsController.js
+++ controllers/aboutUsController.js
@@ -7,7 +7,7 @@ var async = require('async');
 //Display unique page details for Verification
 exports.aboutUs_view = function(req, res) {
   console.log("view Verification Process requested");
-  let aboutUs_1 = "<pre style='position:relative; left:50px; color:slategray;  background:gainsboro; width:650px; padding:10px; align:center'> \n" +
+  let aboutUs_1 = "<pre style='position:relative; left:50px; color:black;  background:gainsboro; width:650px; padding:10px; align:center'> \n" +
   "  This site is the outcome of having taught junior & senior high school students \n" +
   "  for many years and having noticed that for those who have had difficulty it can \n"+
   "  usually be traced back to some critical details that were not clearly understood \n" +
@@ -15,20 +15,22 @@ exports.aboutUs_view = function(req, res) {
   "  undetected and not remedied then the outcome is quite naturally, at the very best, \n" +
   "  mediocre performance in math and at worst an active dislike of mathematics!\n" +
   "  \n"+
-  "  There can surely be no worse outcome for a student's personal, and academic future than \n"+
-  "  to prematurely completely discard an entire branch of study, be it Arts, Science or \n"+
-  "  whatever.\n" +
-  "  The intent of the modules made availbale on this site is to simply avoid such outcomes\n" +
-  "  by trying to redress any shortcomings that may have popped up by presenting some essential\n" +
-  "  and key basic math concepts in a way that will hopefully make them more interesting and\n" +
-  "  memorable.  This is achieved by using a more impactful visual presentation coupled with\n"+
-  "  interactive engagement in the learning stage and by immediately following this with graded\n"+
-  "  problems linked to the material and immediate feedback on the student's performance on these."
+  "  There can surely be no worse outcome for a student's personal, and academic future \n"+
+  "  than to prematurely discard an entire branch of study, be it Arts, Science or whatever! \n"+
+  "  \n"+
+  "  The intent of this site then is to simply prevent such outcomes by hopefully avoiding\n" +
+  "  them in the first place, or if the need arises, remedy any that may have cropped up.\n" +
+  "  Our modules present fundamental math concepts in a way that we believe student's will \n" +
+  "  find interesting and memorable.  This is achieved by using a more impactful visual \n"+
+  "  presentation coupled with active engagement in the learning stage followed immediately\n"+
+  "  with problems relevant to the materail at hand and giving immediate feedback on the \n"+
+  "  student's performance on these; all within the same module."
 
-  let aboutUs_2 =  "There is but one of \"Us\" in About Us and I would hope that all\n"+
-  "   visitors to this site will keep that in mind should they find our effort to be\n"+
-  "   of use in their effort to inculcate a fondness for mathematics in their child(ren)\n"
-  "   or the students in their charge.";// `<a href='/pieSlicerDwnld'><span style='color:white; background:black'>DOWNLOAD DEMO</span>`;
+  let aboutUs_2 =  "There is but one of \"Us\" in About Us and I would hope that all visitors to this\n"+
+  "   site will keep that in mind and should they find our modules to be of use in their efforts\n"+
+  "   to inculcate a fondness for mathematics in their child(ren) or the students in their charge\n"+
+  "   they will consider helping our efforts by donating an amount commensurate to the help we\n"+
+  "   we have been able to provide and hope to continue doing so with newer and better modules.";// `<a href='/pieSlicerDwnld'><span style='color:white; background:black'>DOWNLOAD DEMO</span>`;
 
 res.render('aboutUs_view', { title: "About Us",
                                  themeDesc1: aboutUs_1,
