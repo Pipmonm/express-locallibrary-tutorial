538473a5b7a73c3e85efa60fd7f0ca1460ceb523
Pipmon <pipmonm@netscape.net>
2019-01-22 20:08:03 -0500

    Adding SHA-256 hash codes part 3

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -52,7 +52,8 @@ exports.verify_view = function(req, res) {
   let filename = "step" + VerifyState.step.toString() + ".png";
   console.log("@@@ $ looking for : " + filename);
   //line to force update
-  let image = '../public/images/'+ filename; //this provides download href
+  //let image = '../public/images/'+ filename; //this provides download href
+  let image = 'https://s3.ca-central-1.amazonaws.com/pipsverifybucket/' + filename;
   let imageTitle = 'Step ' + VerifyState.step.toString() + ' image';
   let nextLocation = '/catalog/verification';
   let prevLocation = '/catalog/backVerify';
