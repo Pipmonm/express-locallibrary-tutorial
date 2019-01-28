31f1f678ad35fd0881770653419ff47c8b2ee463
Pipmon <pipmonm@netscape.net>
2019-01-28 14:24:00 -0500

    stepping through demo instruction

--- controllers/downloadPSController.js
+++ controllers/downloadPSController.js
@@ -16,12 +16,12 @@ exports.downloadPS_view = function(req, res) {
   "bottom of this page and running the demo from your desktop after verifying the download.\n"+
   "\n"+
   "Verifying the downloaded file before running it is recommended to ensure the file's\n"+
-  "authenticity and integrity.   Using Window's PowerShell utility you can easily compare the\n"+
-  "demo file's SHA-256 hash code (given below) with the one generated for the downloaded file\n"+
-  "as explained on this site's VERIFICATION menu item.\n"+
+  "authenticity and integrity.   Using Window's PowerShell utility you can easily compare \n"+
+  "the file's SHA-256 hash code (given below) with the one generated for the downloaded\n"+
+  "file as explained on this site's VERIFICATION menu item.\n"+
   "\n"+
   "Running the demo:\n"+
-  "After verifying the download run the program by simply double-cliking on it.\n"+
+  "After verifying it run the program from your desktop by double-cliking on it.\n"+
   "\n" +
   "PieSlicerDual.exe (vrs. 1.0.0.3)  file Verification code:\n" +
   "SHA-256 Hash Code:   F889CC16B94F51D2A29726773175172D772C9000C168D7B8247552D6F6D239AA \n" +
