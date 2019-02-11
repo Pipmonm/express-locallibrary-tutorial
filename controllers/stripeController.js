1132baeb7995755373420ebe6846c55846e665c7
Pipmon <pipmonm@netscape.net>
2019-02-11 13:52:08 -0500

    Integrating Stripe part 2 adding STRIPE API

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,3 +1,7 @@
+const keyPublishable = process.env.STRIPE_PUBLISHABLE_KEY;//2019-02-10 added for STRIPE integration
+const keySecret = process.env.STRIPE_SECRET_KEY;//2019-02-10 added for STRIPE integration
+const stripe = require("stripe")(keySecret);//2019-02-10 added for STRIPE integration
+
 //2019-02-10  bare bones to start with
 exports.stripeGet = (req, res) =>
   res.render("stripe_get.pug", {keyPublishable:keyPublishable});//2019-02-11 final version?
