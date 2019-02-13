0cbdffee4e4248303180c4940383b5fb477dca3a
Pipmon <pipmonm@netscape.net>
2019-02-13 17:10:35 -0500

    direct route for checkout /charge

--- routes/stripeAcknowledge.js
+++ routes/stripeAcknowledge.js
@@ -1,7 +1,10 @@
 //2019-02-13 entirely new module for Stripe Checkout
 var express = require('express');
 var router = express.Router();
+var stripe_controller = require('../controllers/stripeController');//2019-02-10 payment tab
 
 console.log("@@@ $$ not too sure from here");
 
-router.get('/charge',verification_controller.verify_start);
+router.get('/charge',stripe_controller.stripePost);
+
+module.exports = router;
