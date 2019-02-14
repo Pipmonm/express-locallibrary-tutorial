f35bed4867d203282efcd12c95636d901540b41e
Pipmon <pipmonm@netscape.net>
2019-02-13 19:09:17 -0500

    checking if variable amounts can be used

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,10 +1,14 @@
 //const keyPublishable = process.env.STRIPE_PUBLISHABLE_KEY;//2019-02-10 added for STRIPE integration
 //const keySecret = process.env.STRIPE_SECRET_KEY;//2019-02-10 added for STRIPE integration
 //const stripe = require("stripe")(keySecret);//2019-02-10 added for STRIPE integration
-let amount = STRIPE.stripeCharge.toString();//2019-02-13 must be a penny amount
-let fancyAmount = "$" + `(amount/100)`.toString();
+//let amount = STRIPE.stripeCharge.toString();//2019-02-13 must be a penny amount
+//let fancyAmount = "$" + `(amount/100)`.toString();
+//**************curiously STRIPE is not defined outside of exported functions
+//               ie only find STRIPE when wherever imported????
 //2019-02-10  bare bones to start with
 exports.stripeGet = (req, res) => {
+  let amount = STRIPE.stripeCharge.toString();//2019-02-13 must be a penny amount
+  let fancyAmount = "$" + `(amount/100)`.toString();
   console.log("@@@ $$ keyPublishable reported as: " + fancyAmount);
   //const keyPublishable = process.env.STRIPE_PUBLISHABLE_KEY; //2019-02-12 try directly (async???)
   res.render("stripe_get.pug", {keyPublishable:'pk_test_5uHse6DFoVXDYSj8H3l1dYvY', amount:amount});//STRIPE.stripeCharge.toString()});//2019-02-11 final version?
@@ -12,6 +16,8 @@ exports.stripeGet = (req, res) => {
 
                                                //using variable seems to cause trouble
 exports.stripePost = (req, res) => {
+  let amount = STRIPE.stripeCharge.toString();//2019-02-13 must be a penny amount
+  let fancyAmount = "$" + `(amount/100)`.toString();
   //let amount = stripeCharge;//2019-02-11 was 500 pennies (number not string)
   console.log("@@@ $ am at stripePost & stripeCharge is: " + STRIPE.stripeCharge + "  or (fancier): " + fancyAmount);
   stripe.customers.create({
