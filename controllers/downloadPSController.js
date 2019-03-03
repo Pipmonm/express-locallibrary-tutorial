bed1424fd9fec0ab999e433d9c86ffb2f7f5fda0
Pipmon <pipmonm@netscape.net>
2019-03-03 13:09:25 -0500

    overflow=auto fix

--- controllers/downloadPSController.js
+++ controllers/downloadPSController.js
@@ -10,7 +10,7 @@ var async = require('async');
 //Display unique page details for PieSlicer
 exports.downloadPS_view = function(req, res) {
   console.log("@@@ $ details of PieSlicer download");
-  let downloadPSDesc1 = "< style='color:yellow; background:green; '> \n" +
+  let downloadPSDesc1 = "<p style='color:yellow; background:green; '> \n" +
   "Dowloading PieSlicerDual Demo:  \n"+
   "Download the demo by clicking on the \"Download Demo\" button shown below.  Placing it\n"+
   "on your desktop will make it easy to \"verify the download\" as described next.\n"+
@@ -26,7 +26,7 @@ exports.downloadPS_view = function(req, res) {
   "\n" +
   "PieSlicerDual.exe (vrs. 1.0.0.3)  file Verification code:\n" +
   "SHA-256 Hash Code:   F889CC16B94F51D2A29726773175172D772C9000C168D7B8247552D6F6D239AA \n" +
-  "(copy & use this code in the \"VERIFICATION\" steps after downloading the demo file.)"
+  "(copy & use this code in the \"VERIFICATION\" steps after downloading the demo file.),</p>"
 
   let blurb = "USB Version: \n" +
   "The application may also be placed on a USB drive allowing use on any computer. \n" +
