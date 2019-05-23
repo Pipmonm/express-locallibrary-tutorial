a2f00fcdc6c8f928dac7e525ba69bd7d36a86a1f
Pipmon <pipmonm@netscape.net>
2019-05-23 17:31:07 -0400

    Updating Status on successful transaction

--- controllers/downloadPSController.js
+++ controllers/downloadPSController.js
@@ -25,8 +25,8 @@ exports.downloadPS_view = function(req, res) {
   " Once verified run the program from your desktop by double-cliking on it.  While in demo mode"+
   " it is recommended that you follow the 'quick tour' instructions as described at startup.<br />"+
   "<br />"+
-  "PieSlicerDual.exe (vrs. 1.0.0.3)  file SHA-256 Verification code:<br />" +
-  "F889CC16B94F51D2A29726773175172D772C9000C168D7B8247552D6F6D239AA<br />" +
+  "PieSlicerDual.exe (vrs. 1.0.0.4)  file SHA-256 Verification code:<br />" +
+  "74A6E534F95E9B4F9FA58C344964F2A28DC86561529F276E66C626C76F0EFF60<br />" +
   "(copy & use this code in the \"VERIFICATION\" steps after downloading the demo file.),</p></div>"
 
 
