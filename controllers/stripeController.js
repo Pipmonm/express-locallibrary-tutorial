ab1bce912f9868375968c2d6eae016d32396542a
Pipmon <pipmonm@netscape.net>
2019-06-10 16:05:42 -0400

    mismatched bracket on CTAUpdate form

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -63,7 +63,7 @@ exports.stripePrePay_post = [
      var sysIdString = req.body.sysIdString;
      console.log("@@@ $ received prePay request for: " + sysIdString );
      if(sysIdString === "simpleTest"){//2019-06-03 WORKING HERE
-        res.redirect('/catalog/countrytaxauthoritycreate');
+        res.redirect('/catalog//countrytaxauthority/:id');
         //added line to force recompilation
         return;
      }
