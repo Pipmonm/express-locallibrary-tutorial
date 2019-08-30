52ba342c424df6756c888ab4768f5553a80704b2
Pipmon <pipmonm@netscape.net>
2019-08-29 22:14:41 -0400

    updating pug files for new values re. amounts and arrays history

--- models/countryTaxAuthority.js
+++ models/countryTaxAuthority.js
@@ -15,16 +15,16 @@ var CountryTaxAuthoritySchema = new Schema(
     current_count: {type: Number, required: false, default:0},//# of transactions during this period
     amount_limit: {type: Number, required:true},//$ limit
     current_year_amount: {type: Number, required:true, default:0},//actual amount amassed THIS  YEAR
-    previous_years_amounts: {type: Array, required:false, default:[0,0,0]},//record keeping
+    previous_years_amounts: {type: Array, required:true, default:[0,0,0]},//record keeping
                                                       //required for tax purposes (confirm with banking records)
-    current_quarter_amount: {type: Number, required:false, default:0},//for current quarter
-    last_three_quarters_array: {type: Array, required:false, default:[0,0,0]},//for tracking last four quarters (for sales taxes limit)
-    current_four_quarters_amount: {type: Number, required:false, default:0},//keep tally of last four quarters
-    previous_quarters_amounts: {type: Array, required: false, default:[0,0]},//keep records
+    current_quarter_amount: {type: Number, required:true, default:0},//for current quarter
+    last_three_quarters_array: {type: Array, required:true, default:[0,0,0]},//for tracking last four quarters (for sales taxes limit)
+    current_four_quarters_amount: {type: Number, required:true, default:0},//keep tally of last four quarters
+    previous_quarters_amounts: {type: Array, required: true, default:[0,0]},//keep records
     transaction_period_type: {type: String, required:false},//restart per month,year???
-    for_period_index: {type: Number, required:false, default:0},//to simplify quaters cycling
+    for_period_index: {type: Number, required:true, default:0},//to simplify quaters cycling
     transaction_date: {type: Date, default: Date.now},
-    attempted: {type:Number, required:false} //keep track of rejected requests
+    attempted: {type:Number, required:false, default:0} //keep track of rejected requests
   }
 );
 
