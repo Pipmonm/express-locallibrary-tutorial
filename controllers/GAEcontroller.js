0a01cfcd66e7fcf0f862d2600221fa1fdf84db03
Pipmon <pipmonm@netscape.net>
2020-04-11 15:29:15 -0400

    implement online feature 7

--- controllers/GAEcontroller.js
+++ controllers/GAEcontroller.js
@@ -2,10 +2,10 @@ exports.go_online_GAE = function (req, res) { //2019-02-24 added this controller
 //let fs = require('fs');
 //let path = require('path');
 console.log("@@@ $ transfer to GAE");
-let blurb = "<pre style='color:yellow; background:green; width:650px'>\n"+
+let blurb = "<pre style='color:yellow; background:green; width:680px'>\n"+
 "Versions of PieSlicer and Fraction Speller are available online on a subscription basis.\n"+
 "A time limited demo is accessible from the site.\n"+
-"Enter demo mode  by login as username:Guest and password:Guest.\n"+
+"Enter demo mode by loging in as username:Guest and password:Guest.\n"+
 "The application, which initially comes up as a fraction utility, has various options \n"+
 "specified in tabs at the bottom.  The PieSlicer & Fraction Speller are accessible \n"+
 "by clicking on the 'EXPLORE' tab.   Instructions on how to proceed are given there.\n"+
