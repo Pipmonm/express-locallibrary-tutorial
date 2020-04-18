9614f4cf7406ca07042f1cc7fbcb11ba3672819c
Pipmon <pipmonm@netscape.net>
2020-04-18 18:40:29 -0400

    move basic vid to pieSlcr tab

--- controllers/pieSlicerController.js
+++ controllers/pieSlicerController.js
@@ -45,7 +45,7 @@ exports.pieSlicer_view = function(req, res) {
                       "30% {background-color: #1c87c9;}"+
                     "}"+
                     "</style>"+
-                    "Watch our video an PieSlicer's more advanced features: <a id='vid1' href='https://drive.google.com/open?id=1c-2o6268KGJALIXX28_TnIs4j8Ff78Cl'  target='_blank'>..HERE..</a><br />"+
+                    "Watch PieSlicer's basics video <a id='vid_basic' href='https://drive.google.com/open?id=1c-2o6268KGJALIXX28_TnIs4j8Ff78Cl'  target='_blank'>..HERE..</a> and video on more advanced features: <a id='vid_advanced' href='https://drive.google.com/open?id=1c-2o6268KGJALIXX28_TnIs4j8Ff78Cl'  target='_blank'>..HERE..</a><br />"+
                     "Download the Demo version and experiment with it ( no obligations ).<br />"+
                     "The demo will be functional for a limited time only.<br />" +
                     "Buying the license to convert the demo to the unlimited version is detailed"+
