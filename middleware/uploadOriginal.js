0016fae8e3ff8f7fc262ca66bcc6f1177f43a8a0
Pipmon <pipmonm@netscape.net>
2020-05-21 17:21:05 -0400

    bypassing constructor for var storage (multer-gridfs-storage)

--- middleware/uploadOriginal.js
+++ middleware/uploadOriginal.js
@@ -1,13 +1,27 @@
 const util = require("util");
 const multer = require("multer");
+//################### special test ######################
+//Import the mongoose module
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB_URI || "mongodb://Pipmon:MLBsfae!001@ds231090.mlab.com:31090/pipmongodb"
+//mongoDB definition above to check out why environment variable set below (with heroku set config) doesn't work!
+//Set up default mongoose connection
+//var mongoDB = 'mongodb://127.0.0.1/my_database';//ORIGINAL DB connection
+//var mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/my_database';//2018=05-19 default to local if online not available
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 //^^^^^^^^^^^^^^^^^^^ end special test ^^^^^^^^^^^^^^^^^^^
-//const GridFsStorage = require("multer-gridfs-storage");//({db:db});//2020-05-21 added extra bracket ({db:db})
+const GridFsStorage = require("multer-gridfs-storage");//({db:db});//2020-05-21 added extra bracket ({db:db})
 
 console.log("@@@ $ in the middleware!");
-//var storage = new GridFsStorage({
-var storage = require("multer-gridfs-storage")({
+var storage = new GridFsStorage({
   url: "mongodb://Pipmon:MLBsfae!001@ds231090.mlab.com:31090/pipmongodb", //for cloud need to set to mlab database
   //options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
