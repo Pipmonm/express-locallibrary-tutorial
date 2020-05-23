eb1bc0d51896c9d4434cf47f1e0c460476070bca
Pipmon <pipmonm@netscape.net>
2020-05-23 14:23:24 -0400

    modded middleware with promisify on upload fn.

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,47 +1,44 @@
+
+
 const util = require("util");
 const multer = require("multer");
-//################### special test ######################
-//Import the mongoose module
-var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI || "mongodb://Pipmon:MLBsfae!001@ds231090.mlab.com:31090/pipmongodb"
-//mongoDB definition above to check out why environment variable set below (with heroku set config) doesn't work!
-//Set up default mongoose connection
-//var mongoDB = 'mongodb://127.0.0.1/my_database';//ORIGINAL DB connection
-//var mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/my_database';//2018=05-19 default to local if online not available
-mongoose.connect(mongoDB);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-//var db = mongoose.connection;
-const mongoURI = "mongodb://Pipmon:MLBsfae!001@ds231090.mlab.com:31090/pipmongodb";
-const db = mongoose.createConnection(mongoURI, { useNewUrlParser: true });
+
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
 //^^^^^^^^^^^^^^^^^^^ end special test ^^^^^^^^^^^^^^^^^^^
-const GridFsStorage = require("multer-gridfs-storage");//({db:db});//2020-05-21 added extra bracket ({db:db})
-
-console.log("@@@ $ in the middleware!");
-var storage = new GridFsStorage({
+//const GridFsStorage = require("multer-gridfs-storage");//({db:db});//2020-05-21 added extra bracket ({db:db})
+var uploadFilesMiddleware = (req, res)=>{
+console.log("@@@ $ in the middleware! & req.params is: ",req.params);
+//for(var item in req)console.log("** req.",item);
+//const storage = new GridFsStorage({
+var storage = require("multer-gridfs-storage")({
   url: "mongodb://Pipmon:MLBsfae!001@ds231090.mlab.com:31090/pipmongodb", //for cloud need to set to mlab database
-  //options: { useNewUrlParser: true, useUnifiedTopology: true },
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
     const match = ["image/png", "image/jpeg"];
-    console.log("@@@ !!! in check file mime type")
+    console.log("@@@ !!! in check file mime type for file: ",file);
     if (match.indexOf(file.mimetype) === -1) {
       const filename = `${Date.now()}-ALaCarte-${file.originalname}`; //alacarte was bezkoder
       console.log("@@@ !!! not image file! returning filename: ",filename);
       return filename;
     }
 
-    console.log("@@@ !! about to return filename + date " )
+    console.log("@@@ !! about to return filename: ",file.originalname);
     return {
       bucketName: "photos",
       filename: `${Date.now()}-ALaCarte-${file.originalname}`
     };
   }
-});
-console.log("@@@ %% entering multer store section");
+});//end storeImage
+console.log("@@@ %% entering multer store with req items:");
+var item;
+for(item in req)console.log("### req.",item);
+//for(var item in file)console.log("~~~ list file.",item);
+console.log("@@@ storage.db: ",storage.db, "  & storage._file: ", storage._file);
 var uploadFile = multer({ storage: storage }).single("file");
-var uploadFilesMiddleware = util.promisify(uploadFile);
+//var testRun = util.promisify(uploadFile)
+//testRun()
+util.promisify(uploadFile());
+};
+//var uploadFilesMiddleware = util.promisify(uploadFile);
+//var uploadFilesMiddleware = ()=>console.log("@@@ $%# so long sucker!, typeof storage is: ",typeof storage);
 module.exports = uploadFilesMiddleware;
