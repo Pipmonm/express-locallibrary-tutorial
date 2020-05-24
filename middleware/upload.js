3a22240da4eca0da432c80fe8430342d179eba4f
Pipmon <pipmonm@netscape.net>
2020-05-23 21:33:31 -0400

    back to first base with a promisify on upload middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -8,7 +8,7 @@ var mongoDB = process.env.MONGODB_URI || "mongodb://Pipmon:MLBsfae!001@ds231090.
 //Bind connection to error event (to get notification of connection errors)
 //^^^^^^^^^^^^^^^^^^^ end special test ^^^^^^^^^^^^^^^^^^^
 //const GridFsStorage = require("multer-gridfs-storage");//({db:db});//2020-05-21 added extra bracket ({db:db})
-var uploadFilesMiddleware = (req, res)=>{
+var uploadFilesMiddleware =util.promisify((req, res)=>{
 console.log("@@@ $ in the middleware! & req.params is: ",req.params);
 //for(var item in req)console.log("** req.",item);
 //const storage = new GridFsStorage({
@@ -41,12 +41,12 @@ console.log("@@@ %% entering multer store with req items:");
 var item;
 for(item in req)console.log("### req.",item);
 //for(var item in file)console.log("~~~ list file.",item);
-console.log("@@@ storage.db: ",storage.db);
+console.log("@@@ storage type: ",typeof storage);
 var uploadFile = multer({ storage: storage }).single("file");
 //var testRun = util.promisify(uploadFile)
-//testRun()
-util.promisify(uploadFile());
-};
+uploadFile(); //last mod  attempt to execute upload
+// promisify is on the exported function only:::   util.promisify(uploadFile());
+});//teminate the mess
 //var uploadFilesMiddleware = util.promisify(uploadFile);
 //var uploadFilesMiddleware = ()=>console.log("@@@ $%# so long sucker!, typeof storage is: ",typeof storage);
 module.exports = uploadFilesMiddleware;
