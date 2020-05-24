75cc31b8c248415279a0d42ade08277d592512f1
Pipmon <pipmonm@netscape.net>
2020-05-24 11:45:10 -0400

    undefined console variable in file:{}

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,20 +1,21 @@
 const upload = require("../middleware/upload");
 
 const uploadFile = async (req, res) => {
-  console.log("@@@ ### entering try for upload of file: ");
-  console.log("## req.params.",req.params);
+  console.log("@@@ ### web: upload cntroller entering try for upload of file: ");
+  console.log("## web: upload controller req.params.",req.params);
   try {
-    for(item in req)console.log("### in controller req.",item);
-    if(req.file != undefined){console.log("### @@@ in controller req.file: ",req.file);}else{
-      console.log("@@@ in controller req.file undefined!");
+    for(item in req)console.log("### in web: upload controller req.",item);
+    if(req.file != undefined){console.log("### @@@ web: in upload controller req.file: ",req.file);}else{
+      console.log("@@@ in web: upload controller req.file undefined!");
     }
 
     await upload(req, res);
 
+    console.log("@@ ## web: upload controller req.file: ",req.file);
     if (req.file == undefined) {
       return res.send(`You must select a file.`);
     }
-    console.log("@@@ ### file loaded successfully req.file: ",req.file);
+    console.log("@@@ ### web: upload controller, file loaded successfully req.file: ",req.file);
     return res.send(`File has been uploaded.`);
   } catch (error) {
     console.log(error);
