b8490b5e663dde9c6ca6d99e7b9ebc5a8130f27a
Pipmon <pipmonm@netscape.net>
2020-08-07 14:28:21 -0400

    update problem model

--- models/problem.js
+++ models/problem.js
@@ -4,10 +4,15 @@ var Schema = mongoose.Schema;
 
 var ProblemSchema = new Schema(
   {
-    keyCode:{type: String, max: 20, unique: true, required: true},
-    answer:{type: String, max: 200, unique: true, required: true},
-    hint: {type: String, max: 200, unique: true, required: false},
-    example:{type: String, max: 200, unique: true, required: false},
+    keyCode:{type: String, max: 64, unique: true, required: true},
+    other:{type: String, max: 64, unique: false, required:false},
+    grade:{type: String, max:32,unique:false,required:false},
+    pageNumber:{type: String, max: 4, unique:false,required:false},
+    problemNumber:{type: String, max: 4, unique:false,required:false},
+    subSection:{type: String, max: 4, unique:false, required:false},
+    answer:{type: String, max: 200, unique: false, required: false},
+    hint: {type: String, max: 200, unique: false, required: false},
+    example:{type: String, max: 200, unique: false, required: false},
   }
 );
 
@@ -18,12 +23,23 @@ ProblemSchema
   return this.keyCode + ": has hint: " + this.hint.length>0 + "  & example: " + this.example.length > 0;
 })
 
+ProblemSchema
+//concatenated problem name
+.virtual('problem_name')
+.get(function(){
+  var useName = this.grade;
+  if(useName == 'other')useName = this.other;
+  var pString = "Problem-" + useName + "  pg #:" + this.pageNumber + "  prob. #:" + this.problemNumber + "  part:" + this.subSection;
+  console.log("@@@ ## showing problem name: ",pString);
+  return pString;
+});
+
 // Virtual for msg's URL
 ProblemSchema
 .virtual('url')
 .get(function () {
   console.log('problem URL is: ' + this._id);
-  return '/catalog/Problem/' + this._id ;
+  return '/catalog/specificproblem/' + this._id ;
 });
 
 //Export model
